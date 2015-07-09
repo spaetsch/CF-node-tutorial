093b20fcac8d7cbb443acef595a1ca02f65f42e1
Sarah Paetsch <spaetsch@gmail.com>
2015-07-08 21:44:28 -0700

    clean up code

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// New Code
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/nodetest1/data');
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  // YAY
-});
 
 //data model
 var userRecordSchema = new mongoose.Schema({
@@ -32,12 +29,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Make our db accessible to our router
@@ -46,7 +39,6 @@ app.use(function(req,res,next){
     req.model = userRecordModel;
     next();
 });
-
 
 app.use('/', routes);
 app.use('/users', users);
@@ -58,7 +50,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
+// ------ error handlers --------
 
 // development error handler
 // will print stacktrace
@@ -81,6 +73,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -13,23 +13,14 @@ router.get('/helloworld', function(req, res) {
 
 /* GET Userlist page. */
 router.get('/userlist', function(req, res) {
-  console.log("hey");
-
-
-    var db = req.db;
-    var model = req.model;
-    model.find(function (err, docs) {
-        console.log(docs);
-        res.render('userlist', {
-            "userlist" : docs
-        });
+  var db = req.db;
+  var model = req.model;
+  model.find(function (err, docs) {
+    console.log(docs);
+    res.render('userlist', {
+        "userlist" : docs
     });
-
-
-    // var collection = db.get('usercollection');
-    // collection.find({},{},function(e,docs){
-    // });
+  });
 });
-
 
 module.exports = router;
